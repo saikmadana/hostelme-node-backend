1c4d0207b16fbcae4e3865217ad65ce715f0c6f9
sai <saikumar.madana@imaginea.com>
2021-03-22 14:31:21 +0530

    Add test API path

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => {
+app.get('/hey', (req, res) => {
     res.send('The server is up & running!???');
 });
 app.listen((process.env.PORT || port), () => {
