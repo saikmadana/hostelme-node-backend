8d9aa88fc611c2b4f07e7e393844eff2fdeaf81c
sai <saikumar.madana@imaginea.com>
2021-03-23 22:42:29 +0530

    Add testing jest framework

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,23 @@
 import express from 'express';
+import roonRoutes from './controller/room/room.route'
 
 const app = express();
 const port = 3000;
+
+app.use(express.json()); //Used to parse JSON bodies
+app.use(express.urlencoded()); //Parse URL-encoded bodies
+
+// mount all routes on /api path
+app.use('/api', roonRoutes);
+
 app.get('/hey', (req, res) => {
-    res.send('Test the actions integrator!');
+    res.send(getString());
 });
+
+export const getString = () => {
+    return 'Test the actions integrator!'
+}
+
 app.listen((process.env.PORT || port), () => {
     return console.log(`server is listening on this ${port}`);
 }).on('error', (err) => {
