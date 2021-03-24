4a9d92cf94bd8e8ffac5b1eadaaf42e3af18db3b
sai <saikumar.madana@imaginea.com>
2021-03-24 10:04:36 +0530

    Add routing

---
+++ src/controller/room/room.ts
@@ -0,0 +1,20 @@
+const list = () => {}
+
+const get = () => {}
+
+const create = () => {}
+
+const update = () => {}
+
+const remove = () => {}
+
+const load = () => {}
+
+export default {
+    list,
+    create,
+    get,
+    update,
+    remove,
+    load
+}
